70a817ec40fae3528279a5b33e93b0644520a5bb
rakdav <rakdav@inbox.ru>
2025-01-31 21:21:59 +0200

    Mediator

---
+++ ChainsOfResponsibility/Program.cs
@@ -0,0 +1,77 @@
+﻿Architector designer = new Architector();
+Builder carpenters = new Builder();
+Electric whistleblower = new Electric();
+
+designer.SetNextWorker(carpenters).SetNextWorker(whistleblower);
+GiveCommand(designer, "Составить план здания");
+GiveCommand(designer, "Установка дверей");
+GiveCommand(designer, "Монтаж электропроводки");
+GiveCommand(designer, "Облицовка стен");
+
+
+
+void GiveCommand(IWorker worker,string command)
+{
+    string str=worker.Execute(command);
+    if (str == "")
+    {
+        Console.WriteLine("Работа прервана");
+    }
+    else Console.WriteLine(str);
+}
+
+interface IWorker
+{
+    IWorker SetNextWorker(IWorker worker);
+    string Execute(string command);
+}
+abstract class AbsWorker:IWorker
+{
+    private IWorker _nextWorker;
+    public AbsWorker()=>_nextWorker=null;
+
+    public virtual string Execute(string command)
+    {
+        if (_nextWorker != null) return _nextWorker.Execute(command);
+        return string.Empty;
+    }
+    public IWorker SetNextWorker(IWorker worker)
+    {
+        _nextWorker = worker;
+        return worker;
+    }
+}
+class Architector : AbsWorker
+{
+    public override string Execute(string command)
+    {
+        if (command == "Спроектируй дизайн") return "проектирую дизайн";
+        else if(command == "Составить план здания") return "Составляю план здания";
+        return base.Execute(command);
+    }
+}
+class Builder : AbsWorker
+{
+    public override string Execute(string command)
+    {
+        if (command == "Залить фундамент") return "Заливаю фундамент";
+        else if (command == "Возвести стены") return "Возвожу стены";
+        else if(command == "Уложить кровлю") return "Укладываю кровлю";
+        return base.Execute(command);
+    }
+}
+class Electric: AbsWorker
+{
+    public override string Execute(string command)
+    {
+        if (command == "Монтаж электропроводки") 
+            return "Выполняю монтаж электропроводки";
+        else if (command == "Монтаж электрооборудования")
+            return "Выполняю монтаж электрооборудования";
+        else if(command == "Монтаж розеток") 
+            return "Выполняю монтаж розеток";
+        else if (command == "Монтаж осветительных приборов")
+            return "Выполняю монтаж осветительных приборов";
+        return base.Execute(command);
+    }
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,58 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+Designer designer = new Designer();
+Director director = new Director();
+
+Controller mediator=new Controller(designer,director);
+director.GiveCommand("Дизайн");
+designer.ExucuteWork();
+
+interface IMediator
+{
+    void Notify(Employee employee, string msg);
+}
+abstract class Employee
+{
+    protected IMediator Mediator;
+    public Employee(IMediator mediator)=> Mediator = mediator;
+    public void SetMediator(IMediator mediator)=>Mediator = mediator;
+}
+class Officiant : Employee
+{
+    public Officiant(IMediator mediator=null!) : base(mediator){}
+    public void ExucuteWork(string work)
+    {
+        Console.WriteLine("Официант передал кассиру:"+work);
+        Mediator.Notify(this, work);
+    }
+    public void SetWork(bool state)
+    {
+        if(state) Console.WriteLine("Официант занят");
+        else Console.WriteLine("Официант свободен");
+    }
+}
+class Cook : Employee
+{
+    private string text;
+    public Cook(IMediator mediator = null!) : base(mediator) { }
+    public void GiveCommand(string _text)
+    {
+        text= _text;
+        if (text == "") Console.WriteLine("Заказа нет");
+        else Console.WriteLine("Нужно заказать:" + text);
+        Mediator.Notify(this,text);
+    }
+}
+
+class Deployment : Employee
+{
+    public Deployment(IMediator mediator) : base(mediator)
+    {
+
+    }
+    public void Explorer(string _text)
+    {
+        if (_text == "") Console.WriteLine("Заказа нет");
+        else Console.WriteLine("Нужно привести:" + _text);
+    }
+}
